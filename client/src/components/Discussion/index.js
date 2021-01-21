c3659beec1bc98dd852284189f7f5bbbceec6bae
Thomas Nguyen <thomasnguyen546@gmail.com>
2021-01-20 18:42:58 -0700

    Page Layouts

--- client/src/components/Discussion/index.js
+++ client/src/components/Discussion/index.js
@@ -1,19 +1,18 @@
 import React from 'react';
-import logoImg from '../../assets/logo.png'
 
-function About() {
+function Discussion() {
     return (
         <section>
             <div className="page-container">
                 <div id="about-header">
-                <img src={logoImg} width="250" height="300" alt="Fortress of fandom logo" />
                     <div className="section-title">
                         <h2>Fortress of Fandom</h2>
                     </div>
                 </div>
                 <p id="about-text">
-                    Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
-            </p>
+                    This is where we will implement the "Deep Thoughts" sort of thing where we can show previews of comments
+                    and have users login if they want to make a contribution
+                </p>
                 <br>
                 </br>
                 <p id="about-text">
@@ -24,4 +23,4 @@ function About() {
     );
 }
 
-export default About;
+export default Discussion;
