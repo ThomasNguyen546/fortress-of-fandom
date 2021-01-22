ee6a3429c3e7fa8bb8a270baa93b9860c3efd152
Aztec <salgorog@hotmail.com>
2021-01-21 21:46:25 -0700

    back end structure

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -4,32 +4,32 @@ const { gql } = require('apollo-server-express');
 // create our typeDefs
 const typeDefs = gql`
 
-type Comment {
+  type Comment {
   _id: ID
   commentText: String
   createdAt: String
   username: String
-}
+  }
 
-type Query {
+  type Query {
   users: [User]
     user(username: String!): User
     comments(username: String): [Comment]
     comment(_id: ID!): Comment
-}
+  }
 
-type User {
+  type User {
   _id: ID
   username: String
   password: String
   email: String
   Comments: [Comment]
-}
+  }
 
-type Mutation {
+  type Mutation {
   login(email: String!, password: String!)
   addUser(username: String!, email: String!, password: String!): Auth
-}
+  }
 `;
 
 // export the typeDefs
