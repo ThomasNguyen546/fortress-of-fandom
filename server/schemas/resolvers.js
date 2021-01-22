0e6331d9b67e9edc55e5e2109edcd53d842d9ae8
Aztec <salgorog@hotmail.com>
2021-01-21 21:44:29 -0700

    back end structure

---
+++ server/schemas/resolvers.js
@@ -0,0 +1,52 @@
+const { User, Comments } = require('../models');
+const { AuthenticationError } = require('apollo-server-express');
+const { signToken } = require('../utils/auth');
+
+const resolvers = {
+  Query: {
+    // get all users
+    users: async () => {
+      return User.find()
+        .select('-__v -password')
+        .populate('comments');
+    },
+    // get a user by username
+    user: async (parent, { username }) => {
+      return User.findOne({ username })
+        .select('-__v -password')
+        .populate('comments');
+    },
+    Comments: async (parent, { username }) => {
+      const params = username ? { username } : {};
+      return Comments.find(params).sort({ createdAt: -1 });
+    },
+    Comment: async (parent, { _id }) => {
+      return Comments.findOne({ _id });
+    }
+  },
+  Mutation: {
+    addUser: async (parent, args) => {
+      const user = await User.create(args);
+      const token = signToken(user);
+
+      return { token, user }
+    },
+    login: async (parent, { email, password }) => {
+      const user = await User.findOne({ email });
+
+      if (!user) {
+        throw new AuthenticationError('Incorrect credentials');
+      }
+
+      const correctPw = await user.isCorrectPassword(password);
+
+      if (!correctPw) {
+        throw new AuthenticationError('Incorrect credentials');
+      }
+      const token = signToken(user);
+      return { token, user };
+    }
+  }
+};
+
+module.exports = resolvers;
---
+++ server/schemas/typeDefs.js
@@ -0,0 +1,36 @@
+// import the gql tagged template function
+const { gql } = require('apollo-server-express');
+
+// create our typeDefs
+const typeDefs = gql`
+
+type Comment {
+  _id: ID
+  commentText: String
+  createdAt: String
+  username: String
+}
+
+type Query {
+  users: [User]
+    user(username: String!): User
+    comments(username: String): [Comment]
+    comment(_id: ID!): Comment
+}
+
+type User {
+  _id: ID
+  username: String
+  password: String
+  email: String
+  Comments: [Comment]
+}
+
+type Mutation {
+  login(email: String!, password: String!)
+  addUser(username: String!, email: String!, password: String!): Auth
+}
+`;
+
+// export the typeDefs
+module.exports = typeDefs;
